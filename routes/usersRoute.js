4e4122ea4737e744501cfd3f076bfcd6e4e28d63
eric barrs <yellastar42@yahoo.com>
2020-07-01 10:55:07 -0500

    update

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,7 +4,6 @@ const { checkToken } = require("../utils/checkToken");
 
 const router = express.Router();
 
-router.get("/", list);
 router.post("/", create);
 router.post("/save", checkToken, saveProfile);
 
